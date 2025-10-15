c9aa22d3aab82cb5ef135aeb9a2cdfa88720f74a
Cristhian-Lucero <cristhianlucero1@fpuna.edu.py>
2025-10-14 23:18:54 -0300

    CV

--- Cristhian_Lucero/src/Componentes/Inicio.jsx
+++ Cristhian_Lucero/src/Componentes/Inicio.jsx
@@ -2,6 +2,9 @@ export function Inicio ( {nam, title, recidencia, children, link, git, img} ){
     const click = (url) => {
         window.open(url,"_blank", "noopener,noreferrer")
     }
+    const click2 = () => {
+        window.open(`${import.meta.env.BASE_URL}Cristhian_Lucero_CV.pdf`,"_blank", "noopener,noreferrer")
+    }
     return(
         <article className="cl-inicio">
             <div className="cl-inicio-info">
@@ -22,6 +25,7 @@ export function Inicio ( {nam, title, recidencia, children, link, git, img} ){
                 <div className="cl-inicio-botones">
                     <button className="cl-inicio-btn" onClick={() => click(git)}><img className="cl-inicio-botones-img" src="https://cdn-icons-png.flaticon.com/128/2111/2111432.png"></img> GitHub</button>
                     <button className="cl-inicio-btn" onClick={() => click(link)}><img className="cl-inicio-botones-img" src="https://cdn-icons-png.flaticon.com/128/4401/4401412.png"></img>Linkedin</button>
+                    <button className="cl-inicio-btn" onClick={() => click2()}><img className="cl-inicio-botones-img" src="https://cdn-icons-png.flaticon.com/128/4401/4401412.png"></img>CV</button>
                 </div>
             </div>
             <div className="cl-inicio-img">
