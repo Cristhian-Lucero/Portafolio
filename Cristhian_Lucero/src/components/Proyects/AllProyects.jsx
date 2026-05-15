4de9aba6ff5c1571991cade2a719229a1ecb7111
Cristhian-Lucero <cristhianlucero1@fpuna.edu.py>
2026-05-15 19:40:47 -0300

    maximizar img

--- Cristhian_Lucero/src/components/Proyects/AllProyects.jsx
+++ Cristhian_Lucero/src/components/Proyects/AllProyects.jsx
@@ -1,4 +1,9 @@
+import { useState } from 'react'
+import { createPortal } from 'react-dom'
+
 export function AllProyects({ proyectos, onMouseDown, onMouseUp }) {
+  const [max, setMax] = useState(false)
+  const [imgMax, setImgMax] = useState(null)
   return (
     <>
       {proyectos.map((proyect) => {
@@ -6,6 +11,10 @@ export function AllProyects({ proyectos, onMouseDown, onMouseUp }) {
           <li key={proyect.name}>
             <h2>{proyect.name}</h2>
             <img
+              onClick={() => {
+                setMax(true)
+                setImgMax(proyect.img)
+              }}
               onMouseDown={onMouseDown}
               onMouseUp={onMouseUp}
               draggable="false"
@@ -16,6 +25,21 @@ export function AllProyects({ proyectos, onMouseDown, onMouseUp }) {
           </li>
         )
       })}
+      {max &&
+        createPortal(
+          <div className="div-max">
+            <img
+              onClick={() => {
+                setMax(false)
+                setImgMax(null)
+              }}
+              className="div-max-img"
+              src={imgMax}
+              alt="img"
+            />
+          </div>,
+          document.body,
+        )}
     </>
   )
 }
